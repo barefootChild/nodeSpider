e575f41a8da47bffa889abe1ce7f718a21a7069d
zhaoyongsheng <zhaoyongsheng@eluying.com>
2017-02-08 13:34:15 +0800

    feat: fix code style

--- spider.js
+++ spider.js
@@ -11,56 +11,56 @@ let https = require('https'),
 function spiderMovie(index) {
     https.get('https://movie.douban.com/top250?start=' + index, (res) => {
         let pageSize = 25;
-    let html = '';
-    let movies = [];
-    res.setEncoding('utf-8');
+        let html = '';
+        let movies = [];
+        res.setEncoding('utf-8');
 
-    res.on('data', (chunk) => { html += chunk; });
+        res.on('data', (chunk) => { html += chunk; });
 
-    res.on('end', () => {
-        let $ = cheerio.load(html);
-    $('.item').each(() => {
-        let picUrl = $('.pic img', this).attr('src');
-    let movie = {
-        title: $('.title', this).text(),
-        start: $('.info .star .rating_num', this).text(),
-        link: $('a', this).attr('href'),
-        picUrl: picUrl
-    };
+        res.on('end', () => {
+            let $ = cheerio.load(html);
+            $('.item').each(() => {
+                let picUrl = $('.pic img', this).attr('src');
+                let movie = {
+                    title: $('.title', this).text(),
+                    start: $('.info .star .rating_num', this).text(),
+                    link: $('a', this).attr('href'),
+                    picUrl: picUrl
+                };
 
-    if (movie) {
-        movies.push(movie);
-    }
+                if (movie) {
+                    movies.push(movie);
+                }
 
-    downloadImg('./img/', movie.picUrl);
-});
+                downloadImg('./img/', movie.picUrl);
+            });
 
-    saveData('./data' + (index / pageSize) + '.json', movies);
-});
-}).on('error', (err) => {
+            saveData('./data' + (index / pageSize) + '.json', movies);
+        });
+    }).on('error', (err) => {
         console.log(err);
-});
+    });
 }
 
 function downloadImg(imgDir, url) {
     https.get(url, (res) => {
         let data = '';
-    res.setEncoding('binary');
-    res.on('data', (chunk) => {
+        res.setEncoding('binary');
+        res.on('data', (chunk) => {
         data += chunk;
-});
+    });
 
     res.on('end', () => {
         fs.writeFile(imgDir + path.basename(url), data, 'binary', (err) => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log('Image downloaded: ', path.basename(url));
-});
-}).on('error', (err) => {
-        console.log(err);
-});
-});
+            if (err) {
+                return console.log(err);
+            }
+            console.log('Image downloaded: ', path.basename(url));
+        });
+    }).on('error', (err) => {
+            console.log(err);
+        });
+    });
 }
 
 function saveData(path, movies) {
@@ -71,7 +71,7 @@ function saveData(path, movies) {
             return console.log(err);
         }
         console.log('Data saved');
-});
+    });
 }
 
 function *doSpider(x) {
